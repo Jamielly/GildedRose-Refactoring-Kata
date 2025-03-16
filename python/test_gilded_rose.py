3a935fa8acbc3112b5bea66419fb61d8ff8f857a
Jamielly Reis <100716307+Jamielly@users.noreply.github.com>
2025-03-16 14:34:17 -0300

    testeunitario

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,16 +1,76 @@
-# -*- coding: utf-8 -*-
 import unittest
-
 from gilded_rose import Item, GildedRose
 
-
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_regular_item_before_sell_date(self):
+        items = [Item("Regular Item", 10, 20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
-
-        
+        self.assertEqual(items[0].sell_in, 9)
+        self.assertEqual(items[0].quality, 19)
+    
+    def test_regular_item_on_sell_date(self):
+        items = [Item("Regular Item", 0, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].sell_in, -1)
+        self.assertEqual(items[0].quality, 18)
+    
+    def test_regular_item_after_sell_date(self):
+        items = [Item("Regular Item", -1, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].sell_in, -2)
+        self.assertEqual(items[0].quality, 18)
+    
+    def test_quality_never_negative(self):
+        items = [Item("Regular Item", 5, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].sell_in, 4)
+        self.assertEqual(items[0].quality, 0)
+    
+    def test_aged_brie_before_sell_date(self):
+        items = [Item("Aged Brie", 2, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].sell_in, 1)
+        self.assertEqual(items[0].quality, 1)
+    
+    def test_aged_brie_max_quality(self):
+        items = [Item("Aged Brie", 2, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].sell_in, 1)
+        self.assertEqual(items[0].quality, 50)
+    
+    def test_sulfuras_no_change(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 5, 80)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].sell_in, 5)
+        self.assertEqual(items[0].quality, 80)
+    
+    def test_backstage_passes_increase_quality(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].sell_in, 14)
+        self.assertEqual(items[0].quality, 21)
+    
+    def test_backstage_passes_quality_zero_after_concert(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].sell_in, -1)
+        self.assertEqual(items[0].quality, 0)
+    
+    def test_conjured_items_degrade_twice_as_fast(self):
+        items = [Item("Conjured Mana Cake", 3, 6)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(items[0].sell_in, 2)
+        self.assertEqual(items[0].quality, 4)
+    
 if __name__ == '__main__':
     unittest.main()
